312e5f1feb8393aa3f4a20f17d7d8e62adcd503c
Nikhil <nikhil.woodruff@outlook.com>
2021-06-15 21:29:04 +0100

    Go back to fork clone

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from argparse_prompt import PromptParser
 
 if __name__ == "__main__":
-    pass
+    parser = PromptParser(description="A utility to setup scripting tools for this analysis repo.")
+    parser.add_argument("repo-name", help="The title of the analysis (url-safe)")
+    args = parser.parse_args()
