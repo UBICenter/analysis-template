4bc7f889c21c5e2b6f7aa079385fa9af21b3f7ed
Nikhil <nikhil.woodruff@outlook.com>
2021-06-15 21:54:09 +0100

    Working version

--- setup.py
+++
@@ -1,6 +0,0 @@
-from argparse_prompt import PromptParser
-
-if __name__ == "__main__":
-    parser = PromptParser(description="A utility to setup scripting tools for this analysis repo.")
-    parser.add_argument("repo-name", help="The title of the analysis (url-safe)")
-    args = parser.parse_args()
